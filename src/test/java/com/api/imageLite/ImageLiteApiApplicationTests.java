2cf019cbae3ca7f7d5b5ec3b223c04aeea5ceb02
Rafael Aguiar <rafael.aguiar.im@gmail.com>
2024-08-27 09:04:27 -0300

    first commit

---
+++ src/test/java/com/api/imageLite/ImageLiteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.imageLite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageLiteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
